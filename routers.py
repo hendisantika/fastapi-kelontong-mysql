8d42f3327dba6cae74cfd8e31fe684419bc8e35d
hendisantika <hendisantika@yahoo.co.id>
2026-07-18 07:28:27 +0700

    feat: add MerchandiseBase in routers.py🫸🌀✏️📗🐧🐳⬆

--- routers.py
+++ routers.py
@@ -5,3 +5,13 @@ from database import engine
 
 router = APIRouter()
 models.Base.metadata.create_all(bind=engine)
+
+
+class MerchandiseBase(BaseModel):
+    name: str
+    category_id: int
+    price: int
+    stock: int
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
